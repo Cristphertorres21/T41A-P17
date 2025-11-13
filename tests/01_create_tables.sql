f38a935d78e435575558fb4ae123dfc73c6d4efb
Cristphertorres21 <cristophertorresdza@gmail.com>
2025-11-12 20:21:21 -0600

    Update 01_create_tables.sql

--- tests/01_create_tables.sql
+++ tests/01_create_tables.sql
@@ -1,3 +1,8 @@
+CREATE TABLE IF NOT EXISTS productos (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    atributos JSONB
+);
 CREATE TABLE usuarios (
   id SERIAL PRIMARY KEY,
   data JSONB
